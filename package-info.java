18016ff482d08dad836b1a8476e700cb8f102dd6
jaeyoon-lee2 <54957190+jaeyoon-lee2@users.noreply.github.com>
2021-05-10 16:31:12 -0400

    Update package-info.java

--- package-info.java
+++ package-info.java
@@ -1,8 +1,9 @@
 /**
- * The Binary Search program.
+ * The Tower of Hanoi program.
  *
  * @author  Jay lee
  * @version 1.0
- * @since   2021-05-07
+ * @since   2021-05-10
 */
-package ca.mths.ics4u.unit3.unit03.java.binarySearch;
+/** package-info.*/
+package ca.mths.ics4u.unit3.unit04.java.towerOfHanoi;
