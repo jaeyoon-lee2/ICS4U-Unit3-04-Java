0f6051c0cc1840a41788ff3b351f16ef1f6d2d87
jaeyoon-lee2 <54957190+jaeyoon-lee2@users.noreply.github.com>
2021-05-10 16:37:13 -0400

    Update package-info.java

--- package-info.java
+++ package-info.java
@@ -1,9 +1,9 @@
 /**
- * The Tower of Hanoi program.
- *
- * @author  Jay lee
- * @version 1.0
- * @since   2021-05-10
+* The Tower of Hanoi program.
+* 
+* @author  Jay Lee
+* @version 1.0
+* @since   2021-05-10
 */
-/** package-info.*/
+
 package ca.mths.ics4u.unit3.unit04.java.towerOfHanoi;
